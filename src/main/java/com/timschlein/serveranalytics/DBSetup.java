3d64a6159760df421f4b4a8ce0772324bc82c6f8
Tim Schlein <timschlein@kailtim.local>
2017-07-24 00:38:58 -0700

    Moved db set up to new class

---
+++ src/main/java/com/timschlein/serveranalytics/DBSetup.java
@@ -0,0 +1,72 @@
+package com.timschlein.serveranalytics;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.Bukkit;
+
+public class DBSetup {
+	public static Connection connection;
+	public static String host, database, username, password;
+	public static int port;
+	static ConsoleCommandSender console = Bukkit.getConsoleSender();
+
+public static void connect() {
+	host = "198.143.155.98";
+	port = 3306;
+	database = "mc114471";
+	username = "mc114471";
+	password = "4a19d822c3";
+	
+	
+	try {
+		openConnection();
+		createTable();
+//		Statement statement = connection.createStatement();
+	
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	}
+public static void createTable() {
+	String setupTable = "CREATE TABLE IF NOT EXISTS PlayerProfiles " + "(id INTEGER not NULL)";
+	Statement stmt = null;
+	try {
+		stmt = connection.createStatement();
+		stmt.executeUpdate(setupTable);
+	} catch (SQLException e) {
+		System.out.println("Fuck this shit");
+		e.printStackTrace();
+	} 	
+	
+}
+public static void openConnection() throws SQLException, ClassNotFoundException {
+    if (connection != null && !connection.isClosed()) {
+        return;
+    }
+ 
+ 
+        if (connection != null && !connection.isClosed()) {
+            return;
+        } 
+        Class.forName("com.mysql.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://" + host+ ":" + port + "/" + database, username, password);
+    }
+
+public static boolean isConnected() {
+    return (connection == null ? false : true);
+}
+public static void disconnect() {
+    if (isConnected()) {
+        try {
+            connection.close();
+            console.sendMessage("MySQL Database Disconnected");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+	}
+}
--- src/main/java/com/timschlein/serveranalytics/ServerAnalytics.java
+++ src/main/java/com/timschlein/serveranalytics/ServerAnalytics.java
@@ -16,60 +16,19 @@ import org.bukkit.command.CommandSender;
 
 
 public class ServerAnalytics extends JavaPlugin{
-	private Connection connection;
-	private String host, database, username, password;
-	private int port;
+
 	
 	@Override
 	public void onEnable() {
-		host = "198.143.155.98";
-		port = 3306;
-		database = "mc114471";
-		username = "mc114471";
-		password = "4a19d822c3";
-		
-		
-		try {
-			openConnection();
-			createTable();
-//			Statement statement = connection.createStatement();
-		
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
 		getLogger().info("onEnable has been invoked!");
+		DBSetup.connect();
+//		
 	}
-	private void createTable() {
-		String setupTable = "CREATE TABLE IF NOT EXISTS PlayerProfiles " + "(id INTEGER not NULL)";
-		Statement stmt = null;
-		try {
-			stmt = connection.createStatement();
-			stmt.executeUpdate(setupTable);
-		} catch (SQLException e) {
-			System.out.println("Fuck this shit");
-			e.printStackTrace();
-		} 	
-		
-	}
+//	
 	@Override
 	public void onDisable() {
 		getLogger().info("onDisable has been invoked!");
+		DBSetup.disconnect();
 	}
 
-	public void openConnection() throws SQLException, ClassNotFoundException {
-	    if (connection != null && !connection.isClosed()) {
-	        return;
-	    }
-	 
-	    synchronized (this) {
-	        if (connection != null && !connection.isClosed()) {
-	            return;
-	        } 
-	        Class.forName("com.mysql.jdbc.Driver");
-	        connection = DriverManager.getConnection("jdbc:mysql://" + this.host+ ":" + this.port + "/" + this.database, this.username, this.password);
-	    }
-	}
 }
